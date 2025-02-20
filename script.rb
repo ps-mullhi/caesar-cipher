1cf761de298ae9cbceacad7d90ca702c7022c0f2
Puneet Singh <199150760+ps-mullhi@users.noreply.github.com>
2025-02-20 13:39:01 -0800

    Encrypt a string using Caeser-Cipher method
    The caeser_cipher method scrambles a string using the Ceasar Cipher
    method that shifts every letter by the shift_factor amount. The
    method implemented here uses right-shifting.
    
    The method is described here:
    https://en.wikipedia.org/wiki/Caesar_cipher
    
    Just a basic project for testing/learning Ruby Basics.

---
+++ script.rb
@@ -0,0 +1,40 @@
+def is_letter?(char)
+  char.match? /\A[a-zA-Z]*\z/
+end
+
+def is_upper?(char)
+  char == char.upcase
+end
+
+def cipher_char(char, shift_factor, 
+  lower_ascii_bound, upper_ascii_bound)
+  
+  ascii_val = char.ord
+  shift_factor = shift_factor % 26
+  space = upper_ascii_bound - ascii_val 
+  if shift_factor <= space
+    return (ascii_val+shift_factor).chr
+  else
+    leftover = shift_factor - space - 1
+    return (lower_ascii_bound + leftover).chr
+  end
+end
+
+def caesar_cipher(string, shift_factor)
+  string.each_char do |char|
+    if is_letter?(char) == false
+      print char
+    elsif(is_upper?(char))
+      # 65 = A, 90 = Z in ascii
+      print cipher_char(char, shift_factor, 65, 90)
+    else
+      print cipher_char(char, shift_factor, 97, 122)
+    end
+  end
+
+  puts "" # - for inserting newline after prints 
+          # - print doesn't insert newline
+end
+
+caesar_cipher("What a string!", 5)
+caesar_cipher("To be or not to be, That is the question", 104)
