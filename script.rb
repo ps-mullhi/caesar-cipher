d6217b6bb0cc52d3786561af18f1dc277919b4a2
Puneet Singh <199150760+ps-mullhi@users.noreply.github.com>
2025-03-07 16:02:10 -0800

    Fixed style issues using rubocop

--- script.rb
+++ script.rb
@@ -1,22 +1,21 @@
 def is_letter?(char)
-  char.match? /\A[a-zA-Z]*\z/
+  char.match?(/\A[a-zA-Z]*\z/)
 end
 
 def is_upper?(char)
   char == char.upcase
 end
 
-def cipher_char(char, shift_factor, 
-  lower_ascii_bound, upper_ascii_bound)
-  
+def cipher_char(char, shift_factor,
+                lower_ascii_bound, upper_ascii_bound)
   ascii_val = char.ord
-  shift_factor = shift_factor % 26
-  space = upper_ascii_bound - ascii_val 
+  shift_factor %= 26
+  space = upper_ascii_bound - ascii_val
   if shift_factor <= space
-    return (ascii_val+shift_factor).chr
+    (ascii_val + shift_factor).chr
   else
     leftover = shift_factor - space - 1
-    return (lower_ascii_bound + leftover).chr
+    (lower_ascii_bound + leftover).chr
   end
 end
 
@@ -24,7 +23,7 @@ def caesar_cipher(string, shift_factor)
   string.each_char do |char|
     if is_letter?(char) == false
       print char
-    elsif(is_upper?(char))
+    elsif is_upper?(char)
       # 65 = A, 90 = Z in ascii
       print cipher_char(char, shift_factor, 65, 90)
     else
@@ -33,9 +32,9 @@ def caesar_cipher(string, shift_factor)
   end
 
   # for inserting newline after prints. print() doesn't
-  # insert newline.  
-  puts ""
+  # insert newline.
+  puts ''
 end
 
-caesar_cipher("What a string!", 5)
-caesar_cipher("To be or not to be, That is the question", 104)
+caesar_cipher('What a string!', 5)
+caesar_cipher('To be or not to be, That is the question', 104)
